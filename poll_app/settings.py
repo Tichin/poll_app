5d1c73056cbfe4965caaccc54f3530979ee6b9cb
Belle Xu <bellex22@protonmail.com>
2023-09-11 18:00:35 -0400

    wip: database set up

--- poll_app/settings.py
+++ poll_app/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+DATABASE_USER = os.environ['DATABASE_USER']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +82,12 @@ WSGI_APPLICATION = 'poll_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'polls',
+        'USER': DATABASE_USER,
+        'PASSWORD': "myupassword",
+        'HOST': "127.0.0.1",
+        'PORT': '5432',
     }
 }
 
